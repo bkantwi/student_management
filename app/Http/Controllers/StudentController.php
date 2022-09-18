43027ac93d4980f162a8895da0327dae5994050d
bkantwi <bernardantwi2010@gmail.com>
2022-09-18 20:58:09 +0000

    update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -13,7 +14,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::all();
+        return view('student',['students'=>$students,'layout'=>'index']);
     }
 
     /**
@@ -23,7 +25,8 @@ class StudentController extends Controller
      */
     public function create()
     {
-        //
+        students = Student::all();
+        return view('student',['students'=>$students,'layout'=>'create']);
     }
 
     /**
@@ -34,7 +37,14 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $student = new Student();
+        $student->cne = $request->input('cne');
+        $student->firstName = $request->input('firstName');
+        $student->lastName = $request->input('secondName');
+        $student->age = $request->input('age');
+        $student->speciality = $request->input('speciality');
+        $student->save();
+        return redirect('/');
     }
 
     /**
